d3e363fb36f9b8dd8c1a405049d7edaaec05d092
LukeRRT <aumrungruj.t@gmail.com>
2024-02-21 03:47:35 +0000

    Task5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
-int main()
+/**
+ * main - Entry Point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
