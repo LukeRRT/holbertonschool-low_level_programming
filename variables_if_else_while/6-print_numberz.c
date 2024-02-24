d222132c2f57287d13e987504aba926fb311909d
LukeRRT <aumrungruj.t@gmail.com>
2024-02-24 02:51:54 +0000

    Task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all number base 10 but only use putchar
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+
+	return (0);
+}
