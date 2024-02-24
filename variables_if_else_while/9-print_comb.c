2a440f812c3ec8cdbc27547acaa550aa14d9b95b
LukeRRT <aumrungruj.t@gmail.com>
2024-02-24 03:26:28 +0000

    Task9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints number 0-9 but with comma and space after all numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchat(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
