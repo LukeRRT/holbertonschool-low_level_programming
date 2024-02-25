86b59ad30b6475c8255d57abb36851ed01463d51
LukeRRT <aumrungruj.t@gmail.com>
2024-02-25 00:56:26 +0000

    Task0

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Standard library includes
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function prototypes
+void greet(char *name);
+int add(int a, int b);
+int _putchar(char c); // Declaration for _putchar function
+
+// Macro definitions
+#define MAX_SIZE 100
+
+// Global variable declarations
+extern int global_var;
+
+#endif /* MAIN_H */
