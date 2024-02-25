5edc35256123486bfd1e7ef5d5bf7799d79f3d02
LukeRRT <aumrungruj.t@gmail.com>
2024-02-25 03:24:51 +0000

    Task9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,27 +10,23 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
 
-			if (k == 0)
-				_putchar('0');		
+			_putchar(',');
+			_putchar(' ');
 
-
-			if (k < 10 && j != 0)
+			if (k <= 9)
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			}
-			else if (k >= 10)
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
