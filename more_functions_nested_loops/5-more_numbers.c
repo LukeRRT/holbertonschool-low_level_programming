6668d7723baa3af69012593a914d775012b84ac2
LukeRRT <aumrungruj.t@gmail.com>
2024-02-27 01:20:27 +0000

    Task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,17 @@
 void more_numbers(void)
 {
 	int num = 0;
+	int row = 0;
 
-	while (num <= 10)
+	while (row <= 10)
 	{
 		num = 0;
-
 		while (num <= 14)
 		{
 			_putchar(num + '0');
-			num = num + 1;
+			num++;
 		}
 		_putchar('\n');
+		row++;
 	}
 }
