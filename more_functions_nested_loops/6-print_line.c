c4be4808409845b76603876caa147fac180c5cac
LukeRRT <aumrungruj.t@gmail.com>
2024-02-27 01:44:50 +0000

    Task6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines depending on number of n
+ * @n: number of lines
+ *
+ * Return: no return
+ *
+ */
+
+void print_line(int n)
+{
+	if (n >= 1)
+	{
+		while (n >= 1)
+		{
+			_putchar('_');
+			n--;
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('/n');
+}
