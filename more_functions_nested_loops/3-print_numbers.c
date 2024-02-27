726ca65f47ecb6d4767cf3b0c958295ea84ab7ce
LukeRRT <aumrungruj.t@gmail.com>
2024-02-27 00:53:12 +0000

    Task3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number 0-9 follow by new line
+ *
+ * Return: no return
+ *
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		_putchar(num);
+		num = num + 1;
+	}
+	_putchar('\n');
+}
