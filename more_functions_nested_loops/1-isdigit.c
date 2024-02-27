0d58ebc0b38204f45a38b50a0e2467861419f668
LukeRRT <aumrungruj.t@gmail.com>
2024-02-27 00:40:14 +0000

    Task1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - to check the number is 0 - 9
+ * @c: character of numbers
+ *
+ * Return: return 1 if yes, 0 if not
+ *
+ */
+
+int _isdigit(int c);
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
