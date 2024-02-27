55fe78bba4f99e1c554e78e516ab9cc2a09215b4
LukeRRT <aumrungruj.t@gmail.com>
2024-02-27 00:45:19 +0000

    Task2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multipile two integers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: multipiled integer
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
