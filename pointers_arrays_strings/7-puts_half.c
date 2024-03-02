04126c85e56cc05f2387b1578ec6dbb94e0eefa6
LukeRRT <aumrungruj.t@gmail.com>
2024-03-02 06:51:41 +0000

    Task7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str: string
+ *
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int i = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	while (i < length)
+	{
+		if (i >= length / 2)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
