028c282b41183ee6b04e3160a71b479927703874
LukeRRT <aumrungruj.t@gmail.com>
2024-03-02 09:19:28 +0000

    Task10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,36 +9,17 @@
 
 int _atoi(char *s)
 {
-	int result = 0;
 	int sign = 1;
-	int digit;
+	unsigned int num = 0;
 
-	while (*s == ' ')
-		s++;
+	do {
+		if (*s =='-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
 
-	if (*s == '-')
-	{
-		sign = -1;
-		s++;
-	}
-	else if (*s == '+')
-	{
-		s++;
-	}
-
-	while (*s >= '0' && *s <= '9')
-	{
-		digit = *s - '0';
-		
-		if (result > (2147483647 - digit) / 10)
-		{
-			if (sign == 1)
-				return (2147483647);
-			else
-				return (-2147483647);
-		}
-		result = (result * 10) + digit;
-		s++;
-	}
 	return (result * sign);
 }
