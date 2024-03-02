4c57665977a4718268c1c114d0bc97294ef47367
LukeRRT <aumrungruj.t@gmail.com>
2024-03-02 09:02:40 +0000

    Task10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an interger
+ * @s: string
+ *
+ * Return: nothing
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	while (*s == ' ')
+		s++;
+
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		num = (num * 10) + (*s - '0');
+		s++;
+	}
+	return (num * sign);
+}
