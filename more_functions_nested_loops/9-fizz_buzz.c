8e1653cadc0631e31e3c9f510ae83b50551b00b2
LukeRRT <aumrungruj.t@gmail.com>
2024-03-02 04:09:08 +0000

    Task9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	while (n >= 1)
+	{
+		if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+
+		if (n != 100)
+			printf(" ");
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
