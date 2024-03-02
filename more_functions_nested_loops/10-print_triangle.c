7099b8330787f7a48676db9ac0c5f28a14f88757
LukeRRT <aumrungruj.t@gmail.com>
2024-03-02 04:30:00 +0000

    Task10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle using #
+ * @size: number of # in triangle
+ *
+ * Return: no return
+ *
+ */
+
+void print_triangle(int size)
+{
+	int row = 0;
+	int col = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	while (row <= size)
+	{
+		while (col <= size - row)
+		{
+			_putchar(' ');
+			col++;
+		}
+		while (col <= row)
+		{
+			_putchar('#');
+			col++
+		}
+		_putchar('\n');
+		row++;
+	}
+}
