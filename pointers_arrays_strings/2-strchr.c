fb86f6128c8ab5f5a5cc1e26d340a20d87695248
LukeRRT <aumrungruj.t@gmail.com>
2024-03-06 01:22:39 +0000

    Task2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strchr - locate a character in the string
+ * @s: string that need to find character c inside
+ * @c: A character to find
+ *
+ * Return: found character/ NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
