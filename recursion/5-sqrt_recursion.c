6fc701db25532258984e632792358e37c064c953
LukeRRT <aumrungruj.t@gmail.com>
2024-03-09 11:53:05 +0000

    Task5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,13 +8,6 @@
  * Return: square root of a number
  */
 
-int _sqrt_recursion(int n)
-{
-	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt_helper(n, 1));
-}
-
 /**
  * _sqrt_helper - the actual recursive calculation of the natural square root of a number.
  * @n: integer
@@ -31,4 +24,11 @@ int _sqrt_helper(int n, int guess)
 		return (-1);
 	else
 		return (_sqrt_helper(n, guess + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt_helper(n, 1));
 }
