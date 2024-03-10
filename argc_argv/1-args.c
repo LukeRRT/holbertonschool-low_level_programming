264ba060965ec439ab9586e23d3902268f934d75
LukeRRT <aumrungruj.t@gmail.com>
2024-03-10 06:19:39 +0000

    Task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of array
+ * @argv: array of character
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
