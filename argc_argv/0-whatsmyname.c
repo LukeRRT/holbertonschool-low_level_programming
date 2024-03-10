eb0123acce684872d19339cb086eb8839a67ac50
LukeRRT <aumrungruj.t@gmail.com>
2024-03-10 06:14:39 +0000

    Task0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of array
+ * @argv: array of character
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n"), argv[0]);
+	}
+	return (0);
+}
