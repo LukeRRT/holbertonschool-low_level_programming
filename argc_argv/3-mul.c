0dfff852691003fe2993c44d8adb9981e49a4354
LukeRRT <aumrungruj.t@gmail.com>
2024-03-10 06:37:46 +0000

    Task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,7 +12,10 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	int result = num1 * num2;
 
 	if (argc != 3)
 	{
@@ -20,10 +23,6 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-
-	int result = num1 * num2;
 
 	printf("%d\n", result);
 	return (0);
