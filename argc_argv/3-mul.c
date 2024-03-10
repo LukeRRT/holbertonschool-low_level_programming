f05025cf2ca0a37a11319341809457e931d69c8d
LukeRRT <aumrungruj.t@gmail.com>
2024-03-10 06:35:58 +0000

    Task3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of array
+ * @argv: array of character
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	int result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
