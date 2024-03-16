9f482d4d172201cbf0df251e0f0c8e52e4bf7648
LukeRRT <aumrungruj.t@gmail.com>
2024-03-16 07:41:57 +0000

    Task1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *_strdup - copy string to another as parameter
+ * @str: string character
+ *
+ * Return: NULL or character of duplicate
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+
+	duplicate = malloc((strlen(str) + 1) * sizeof(char));
+
+	if (str == NULL || duplicate == NULL)
+		return (NULL);
+
+	strcpy(duplicate, str);
+
+	return (duplicate);
+}
