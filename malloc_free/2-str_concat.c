5f4dee031cbee090fb30627f3c5844e1567b9e91
LukeRRT <aumrungruj.t@gmail.com>
2024-03-16 08:55:42 +0000

    Task3

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -36,22 +36,18 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	result = malloc(len_s1 + len_s2 + 1);
-
 	if (result == NULL)
 		return (NULL);
-
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		result[k] = s1[i];
 		k++;
 	}
-
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 		result[k] = s2[j];
 		k++;
 	}
-
 	result[k] = '\0';
 
 	return (result);
