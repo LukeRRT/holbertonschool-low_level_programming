2a9e9f1ec313ca8999d7dc439968c45b576b35dd
LukeRRT <aumrungruj.t@gmail.com>
2024-03-17 03:34:48 +0000

    Task5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog's memory allocated
+ * @d: dog
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
