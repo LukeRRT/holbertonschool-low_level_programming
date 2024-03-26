bcf2840010ae262c716681ef717a2c8c528f0f9c
LukeRRT <aumrungruj.t@gmail.com>
2024-03-26 07:54:16 +0000

    Task0

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,9 @@
+#ifndef Header_file
+#define Header_file
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int (*get_op_func(char *s))(int, int);
+
+#endif
