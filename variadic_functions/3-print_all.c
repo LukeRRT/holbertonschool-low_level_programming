7a8b74934f93587e41cfbd7210e60731201b586d
LukeRRT <aumrungruj.t@gmail.com>
2024-03-27 03:43:47 +0000

    Task3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,108 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_char - print char
+ * @args: char to be printed
+ *
+ * Return: nothing
+ */
+
+void print_char(va_list *args)
+{
+	char c;
+
+	c = va_arg(*args, int);
+	printf("%c", c);
+}
+
+/**
+ * print_integer - print int
+ * @args: int to be printed
+ *
+ *
+ */
+
+void print_integer(va_list *args)
+{
+	int x;
+
+	x = va_arg(*args, int);
+	printf("%d", x);
+}
+
+/**
+ * print_float - print float
+ * @args: float to be printed
+ *
+ *
+ *
+ */
+
+void print_float(va_list *args)
+{
+	double f;
+
+	f = va_arg(*args, double);
+	printf("%f", f);
+}
+
+/**
+ * print_string - print string
+ * @args: string to be printed
+ *
+ *
+ *
+ */
+
+void print_string(va_list *args)
+{
+	char *str;
+
+	str = va_arg(*args, char*);
+
+	printf("%s", (s == NULL) ? "((nil)" : s);
+}
+
+/**
+ * print_all - print anything
+ * @format: what need to be printed
+ *
+ *
+ *
+ */
+
+void print_all(const char * const format, ...)
+{
+	op_t op[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string}
+	};
+	va_list list;
+	int i = 0, j = 0;
+	int printed_one = 0;
+
+	va_start(list, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		while (j < 4)
+		{
+			if (*ops[j].type == format[i])
+			{
+				if (printed_one == 1)
+					printf(", ");
+
+				ops[j].func(&list);
+				printed_one = 1;
+			}
+			j += 1;
+		}
+		j = 0;
+		i += 1;
+	}
+	va_end(list);
+	printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,15 @@
 #ifndef Header_variadic
 #define Header_variadic
 
+#include <stdarg.h>
+
+typedef struct op
+{
+	char type;
+	void (*func)(va_list *args);
+} op_t;
+
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
