d8e91bc28e2ffb7b0b11af9263eddafa8a7d657b
LukeRRT <aumrungruj.t@gmail.com>
2024-04-11 12:35:45 +0000

    Task0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,7 +17,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buffer;
+	char buffer[letters = 1];
 	ssize_t bytes_read;
 	ssize_t bytes_written;
 
@@ -28,7 +28,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buffer[letters + 1];
 	bytes_read = read(fd, buffer, letters);
 
 	if (bytes_read == -1)
