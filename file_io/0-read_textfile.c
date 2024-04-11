d73af033eeed950fa33afed332ba06dc0ae415ab
LukeRRT <aumrungruj.t@gmail.com>
2024-04-11 12:38:39 +0000

    Task0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,7 +17,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buffer[letters = 1];
+	char *buffer;
 	ssize_t bytes_read;
 	ssize_t bytes_written;
 
@@ -28,17 +28,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	bytes_read = read(fd, buffer, letters);
+	buffer = malloc(letters + 1);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return(0);
+	}
 
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
 		close(fd);
+		free(buffer);
 		return (0);
 	}
 
 	buffer[bytes_read] = '\0';
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	close(fd);
+	free(buffer);
 
 	if (bytes_written == -1 || bytes_written != bytes_read)
 		return (0);
