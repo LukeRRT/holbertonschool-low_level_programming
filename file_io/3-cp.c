a185ce9ee18898697eba5567ba6a38ed981cc0e1
LukeRRT <aumrungruj.t@gmail.com>
2024-04-11 13:13:22 +0000

    Task3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -33,17 +33,23 @@ int main(int argc, char *argv[])
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
 	char buf[BUF_SIZE];
+	struct stat st;
 
 	if (argc != 3)
 		print_error_and_exit(97, "Usage: cp file_from file_to");
+	if (stat(argv[2], &st) == 0 && !S_ISREG(st.st_mode))
+		print_error_and_exit(99, "Error: Can't write to file");
 
 	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		print_error_and_exit(98, "Error: Can't read from file");
+
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	if (fd_from == -1 || fd_to == -1)
-		print_error_and_exit(98, (fd_from == -1) ?
-				"Error: Can't read from file" : "Error: Can't write to file");
-
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		print_error_and_exit(99, "Error: Can't write to file");
+	}
 	while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buf, bytes_read);
@@ -54,13 +60,15 @@ int main(int argc, char *argv[])
 			print_error_and_exit(99, "Error: Can't write to file");
 		}
 	}
-
 	if (bytes_read == -1)
+	{
+		close(fd_from);
+		close(fd_to);
 		print_error_and_exit(98, "Error: Can't read from file");
+	}
 
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 		print_error_and_exit(100, "Error: Can't close file descriptor");
-
 	return (0);
 }
 
